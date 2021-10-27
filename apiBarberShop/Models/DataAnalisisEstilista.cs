27e2653332ef67c62bf20b9f7f7b5bb6324313ac
JORGE PEÑA SANCHEZ <jorge_pena@usmp.pe>
2021-10-26 23:11:35 -0500

    V15

--- apiBarberShop/Models/DataAnalisisEstilista.cs
+++ apiBarberShop/Models/DataAnalisisEstilista.cs
@@ -11,6 +11,8 @@ namespace apiBarberShop.Models
         [Key]
         public int ID { get; set; }
         public int CATE { get; set; }
+        public string DESCRIPCION { get; set; }
+        public string DNI { get; set; }
         public string NOMBRE { get; set; }
         public int SEXO { get; set; }
         public int PROMEDIO { get; set; }
